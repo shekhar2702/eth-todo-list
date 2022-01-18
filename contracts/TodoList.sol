fbd7b7c03c6a33072b17274c702339c4c107c7a7
shekhar2702 <shekhar.prince1234@gmail.com>
2022-01-19 01:50:06 +0530

    Project completed.

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
-
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public tasks;
+    event TaskCreated(uint id,string content,bool completed);
+    constructor() public {
+        createTask("First Task");
+    }
+    function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+  }
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount,_content,false);
+    }
 }
